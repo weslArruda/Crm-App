e7cc0a25c74c0b22df45489fc6e935d5c7d39bf6
weslArruda <weslleyarrudalemospinheiro@outlook.com>
2024-09-08 14:09:49 -0300

    Sales method by Vinn4s {2#}

--- backend/src/main/java/com/saas/jrs/Modules/Entitys/VendaEntity.java
+++ backend/src/main/java/com/saas/jrs/Modules/Entitys/VendaEntity.java
@@ -1,23 +1,11 @@
 package com.saas.jrs.Modules.Entitys;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
-
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data;
-
-@Data
-@Entity(name = "vendas")
 public class VendaEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    private Long id;
 
     private String produto;
     private Double valor;
@@ -27,44 +15,44 @@ public class VendaEntity {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-//    //Gets e Sets
+   //Gets e Sets
    
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//     this.id = id;
-// }
+    public void setId(Long id) {
+    this.id = id;
+}
 
-//     public String getProduto() {
-//     return produto;
-// }
+    public String getProduto() {
+    return produto;
+}
 
-// public void setProduto(String produto) {
-//     this.produto = produto;
-// }
-// public double getValor() {
-//     return valor;
-// }
-// public void setValor(double valor) {
+public void setProduto(String produto) {
+    this.produto = produto;
+}
+public double getValor() {
+    return valor;
+}
+public void setValor(double valor) {
     
-// }
+}
 
-// public String getVendedor() {
-//     return vendedor;
-// }
+public String getVendedor() {
+    return vendedor;
+}
 
-// public void setVendedor(String vendedor) {
-//     this.vendedor = vendedor;
-// }
+public void setVendedor(String vendedor) {
+    this.vendedor = vendedor;
+}
 
-// public LocalDateTime getDataVenda() {
-//     return dataVenda;
-// }
+public LocalDateTime getDataVenda() {
+    return dataVenda;
+}
 
-// public void setDataVenda(LocalDateTime dataVenda) {
-//     this.dataVenda = dataVenda;
-// }
+public void setDataVenda(LocalDateTime dataVenda) {
+    this.dataVenda = dataVenda;
+}
 };
     
--- backend/src/main/java/com/saas/jrs/Modules/Repositories/VendaRepository.java
+++ backend/src/main/java/com/saas/jrs/Modules/Repositories/VendaRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VendaRepository extends JpaRepository<VendaEntity, Long> {
 
-    // Object getVenda();
+    Object getVenda();
 
-    // VendaEntity save(VendaRepository venda);
+    VendaEntity save(VendaRepository venda);
 
 }
