0e0c81b82f13358e4bd605b132575f45941268f3
weslArruda <weslleyarrudalemospinheiro@outlook.com>
2024-09-27 23:42:24 -0300

    test

--- frontend/app/Test/page.tsx
+++ frontend/app/Test/page.tsx
@@ -1,57 +1,117 @@
 'use client'
 
-import { forwardRef } from 'react';
-import { IconChevronRight } from '@tabler/icons-react';
-import { Group, Avatar, Text, Menu, UnstyledButton } from '@mantine/core';
+import { useState, useEffect } from 'react';
+import { IconArchive, IconArrowsLeftRight, IconChevronRight, IconCircleOff, IconLayoutDashboard, IconMessageCircle, IconPencil, IconPhoto, IconSettings, IconStatusChange, IconTrash, IconUserCode } from '@tabler/icons-react';
+import { Group, Avatar, Text, Menu, UnstyledButton, Table, ActionIcon, Badge, Breadcrumbs, Flex, NavLink, rem, Stack } from '@mantine/core';
+import UsersTableApi from '@/components/Tables/UsersTableApi';
+import ColorSchemeToggle from '@/components/ColorSchemeToggle/ColorSchemeToggle';
 
-interface UserButtonProps extends React.ComponentPropsWithoutRef<'button'> {
-  image: string;
-  name: string;
-  email: string;
-  icon?: React.ReactNode;
-}
+// type User = {
+//   id: string;
+//   nome?: string;
+//   username?: string;
+//   senha?: string;
+//   email: string;
+//   nascimento?: string;
+//   cargo?: string;
+//   supervisor?: string;
+//   permissao?: string;
+//   status?: string;
+//   createdAt?: string;
+// };
 
-const UserButton = forwardRef<HTMLButtonElement, UserButtonProps>(
-  ({ image, name, email, icon, ...others }: UserButtonProps, ref) => (
-    <UnstyledButton
-      ref={ref}
-      style={{
-        padding: 'var(--mantine-spacing-md)',
-        color: 'var(--mantine-color-text)',
-        borderRadius: 'var(--mantine-radius-sm)',
-      }}
-      {...others}
-    >
-      <Group>
-        <Avatar src={image} radius="xl" />
+// const fetchData = async () => {
+//   try {
+//     const response = await fetch('http://localhost:8080/user/list');
+//     console.log('Tentando listar usuarios', response)
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Fetch error:', error);
+//     return [];
+//   }
+// };
 
-        <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
-            {name}
-          </Text>
+export default function Page() {
+  // const [elements, setElements] = useState<User[]>([]);
 
-          <Text c="dimmed" size="xs">
-            {email}
-          </Text>
-        </div>
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const data = await fetchData();
+  //     setElements(data);
+  //   };
+  //   getData();
+  // }, []);
 
-        {icon || <IconChevronRight size="1rem" />}
-      </Group>
-    </UnstyledButton>
-  )
-);
+  // const rows = elements.map((element) => (
+  //   <tr key={element.nome}>
+  //     <td><Group justify='center'>{element.id}</Group></td>
+  //     <td><Group justify='center'>{element.nome}</Group></td>
+  //     <td><Group justify='center'>{element.username}</Group></td>
+  //     <td><Group justify='center'>{element.email}</Group></td>
+  //     <td><Group justify='center'>{element.cargo}</Group></td>
+  //     <td><Group justify='center'>{element.supervisor}</Group></td>
+  //     <td><Group justify='center'>{element.permissao}</Group></td>
+  //     <td><Group justify='center'>{element.status}</Group></td>
+  //     <td><Group justify='center'>{element.createdAt}</Group></td>
+  //     <td>
+  //       <Group justify='center'>
+  //         {element.status}
+  //       </Group>
+  //     </td>
+  //     <td>
+  //       <Group justify='center' gap={'xs'}> 
+  //         <ActionIcon variant="default">
+  //           <IconPencil style={{ width: '70%', height: '70%' }} stroke={1.5} />
+  //         </ActionIcon>
+  //         <ActionIcon variant="default">
+  //           <IconStatusChange style={{ width: '70%', height: '70%' }} stroke={1.5} />
+  //         </ActionIcon>
+  //         <ActionIcon variant="default" style={{ background: 'var(--mantine-color-red-9)' }}>
+  //           <IconTrash style={{ width: '70%', height: '70%' }} stroke={1.5} />
+  //         </ActionIcon>
+  //       </Group>
+  //     </td>
+  //   </tr>
+  // ));
 
-export default function page() {
-  return (
-    <Menu withArrow>
-      <Menu.Target>
-        <UserButton
-          image="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-          name="Harriette Spoonlicker"
-          email="hspoonlicker@outlook.com"
-        />
-      </Menu.Target>
-      {/* ... menu items */}
-    </Menu>
-  );
+  // return (
+  //   <Table highlightOnHover>
+  //     <thead>
+  //       <tr>
+  //         <th>Id</th>
+  //         <th>Nome</th>
+  //         <th>Username</th>
+  //         <th>Email</th>
+  //         <th>Cargo</th>
+  //         <th>Supervisor</th>
+  //         <th>Permissão</th>
+  //         <th>Status</th>
+  //         <th>Data de criação</th>
+  //         <th>Ações</th>
+  //       </tr>
+  //     </thead>
+  //     <tbody>{rows}</tbody>
+  //   </Table>
+  // );
+  <>
+  {/* header */}
+  <Flex gap='md'>
+
+      {/* Page content */}
+      <Flex justify='flex-end' direction='column' w='85%'>
+         
+          <Group w='100%'>
+              <UsersTableApi />
+          </Group>
+          
+
+      </Flex>
+  </Flex>
+
+</>
+
 }
