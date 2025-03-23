f72d1b8c686b934ae3edebaaaeccc1c95b346241
weslArruda <weslleyarrudalemospinheiro@outlook.com>
2025-03-22 23:00:37 -0300

    fix: cors get local host address

--- backend/src/main/java/com/saas/jrs/Modules/WebConfig.java
+++ backend/src/main/java/com/saas/jrs/Modules/WebConfig.java
@@ -3,16 +3,24 @@ package com.saas.jrs.Modules;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Adicione o domínio do seu frontend
+        try {
+            String localIp = InetAddress.getLocalHost().getHostAddress();
+            registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://" + localIp + ":3000") // Adicione o domínio do seu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+        } catch (UnknownHostException e ) {
+            e.printStackTrace();
+        }
     }
 }
